859b000018a6d23c591e9c14305839615d26c4cc
Miles Felton Gerber <mwfelton03@gmail.com>
2022-01-09 14:10:08 +0000

    Progress with slider function

--- client-side/src/components/slider-component/Slider.js
+++ client-side/src/components/slider-component/Slider.js
@@ -34,6 +34,51 @@ const Slider = () => {
     <section className="container-slider">
       <div className='sliderBox'>
 
+      <div className="textSide">
+        <h1>What we teach</h1>      
+        <p>The Isha Yoga practices offer an introduction to meditation and Workshops can be tailored for individual needs and medical requirements.
+        </p>
+
+      </div>
+
+      <div className='rightSide'>
+
+      {/* <div className="elements"> */}
+
+      <div className='button'><SliderBtn moveSlide={prevSlide} direction={"prev"}/> </div>
+
+      <div className='slider'>
+        {SliderData.map((obj, index) => {
+            return (
+              <div 
+                key={obj.id}
+                className={slideIndex === index + 1 ? "slide active-anim" : "slide"}>
+                  <img src={process.env.PUBLIC_URL + `/img/slider/img${index + 1}.jpg`} />
+               </div>
+                )
+              })}
+        
+
+      </div>
+
+      <div className='button'><SliderBtn moveSlide={nextSlide} direction={"next"}/> 
+      </div>
+
+    
+      </div>
+
+      </div>
+    {/* </div> */}
+    </section>
+  )};
+
+export default Slider;
+
+// before babus session
+
+{/* <section className="container-slider">
+      <div className='sliderBox'>
+
       <div className='sliderLeft'>
         <h1>Angamardana</h1>      
         <p>Angamardana is a system rooted in yoga that offers to everyone the opportunity to invigorate the body and to reach peak physical and mental health.</p>
@@ -57,23 +102,4 @@ const Slider = () => {
   
     </div>
 
-    </section>
-  )};
-
-export default Slider;
-
-{/* <div className="content1">
-              <div className="content2">
-                <h1>Angamardana</h1>
-                
-                <p>Angamardana is a system rooted in yoga that offers to everyone the opportunity to invigorate the body and to reach peak physical and mental health.</p>
-
-                <p>“Angamardana” means gaining complete mastery over the limbs, organs and other parts of the body. This practice revitalizes the body on all levels including the muscles, blood circulation, skeletal structure, nervous system, and the basic energy system. It strengthens the spine, builds physical strength, fitness and tenacity, taking years off the body.​</p>
-              </div>
-
-              <div className='content3'>
-                <IoIosArrowBack className="forwardBack"/>
-                <img className="sliderImage" src={anga} alt="" />
-                <IoIosArrowForward className="forwardArrow" />
-              </div>
-            </div> */}
+    </section> */}
