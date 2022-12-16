b15cd83d5a93fdbfc8d8d3044db9ef0eda765bcb
Miles Felton Gerber <mwfelton03@gmail.com>
2022-12-16 16:22:16 +0100

    file restructuring

--- client-side/src/routes/sign-in/sign-in-page.component.js
+++ client-side/src/routes/sign-in/sign-in-page.component.js
@@ -1,6 +1,9 @@
+import SignInForm from "../../components/sign-in.component/sign-in.component";
+
 const SignIn = () => (
     <div className="title-container">
         <h1 className="title">SIGN IN</h1>
+        <SignInForm />
     </div>
   );
 
