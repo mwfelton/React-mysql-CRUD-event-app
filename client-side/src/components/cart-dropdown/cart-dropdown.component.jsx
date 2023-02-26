471a15ec6144dd9d2344a98ef305a8ee4ec15242
Miles Felton Gerber <mwfelton03@gmail.com>
2023-02-26 14:48:36 +0000

    add to cart feature added

--- client-side/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ client-side/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,24 +1,20 @@
 import { useContext } from 'react';
 
-// import { CartContext } from '../../contexts/cart.context';
+import { CartContext } from '../../contexts/cart.context';
 
-// import CartItem from '../cart-item/cart-item.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
 const CartDropDown = () => {
-//   const { cartItems } = useContext(CartContext);
+  const { cartItems } = useContext(CartContext);
 
   return (
     <div className='cart-dropdown-container'>
       <div className='cart-items'>
-        {/* {cartItems.length ? (
-          cartItems.map((cartItem) => (
-            <CartItem key={cartItem.id} cartItem={cartItem} />
-          ))
-        ) : (
-          <span className='empty-message'>Your cart is empty</span>
-        )} */}
+          {cartItems.map((item) => (
+            <CartItem key={item.id} cartItem={item} />
+          ))}
       </div>
       <button>GO TO CHECKOUT</button>
     </div>
--- client-side/src/components/workshop-card/workshop-card.component.jsx
+++ client-side/src/components/workshop-card/workshop-card.component.jsx
@@ -1,14 +1,21 @@
 import './workshop-card.styles.scss';
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context'; 
 
 const WorkshopCard = ({ workshop }) => {
     const { name, price, imageUrl } = workshop;
-    return (<div className='workshop-card-container'>
-        <img src={imageUrl} alt="" />
-        <div className='footer'>
-            <span className='name'>{name}</span>
-            <span className='price'>{price}</span>
+    const {addItemToCart} = useContext(CartContext)
+
+    return (
+        <div className='workshop-card-container'>
+            <img src={imageUrl} alt="" />
+            <div className='footer'>
+                <span className='name'>{name}</span>
+                <span className='price'>{price}</span>
+                <button onClick={() => addItemToCart(workshop)}>Add to Cart</button>
+            </div>
         </div>
-    </div>)
+    )
 }
 
 export default WorkshopCard;
