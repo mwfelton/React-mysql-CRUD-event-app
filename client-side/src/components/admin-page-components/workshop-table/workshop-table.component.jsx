1465ffb31299ccd65093e63761c14d6924258c83
Miles Felton Gerber <mwfelton03@gmail.com>
2023-02-28 22:59:00 +0000

    admin page UI improvements - refactoring components

--- client-side/src/components/admin-page-components/workshop-table/workshop-table.component.jsx
+++ client-side/src/components/admin-page-components/workshop-table/workshop-table.component.jsx
@@ -1,30 +1,68 @@
-import './workshop-table.styles.scss';
-import { useState } from 'react';
+import * as React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
 import { doc, deleteDoc} from "firebase/firestore";
 import { db } from "../../../utils/firebase/firebase.utils"
 
-const WorkshopTable = ({ workshop }) => {
-    console.log(workshop)
-    const { country, price, date, id, location, practice, sessions, img } = workshop;
+import './workshop-table.styles.scss';
+
+const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'practice', headerName: 'Practice', width: 130 },
+    { field: 'date', headerName: 'Date', width: 130,  type: 'number' },
+    { field: 'price', headerName: 'Price', type: 'number', width: 90 },
+    { field: 'location', headerName: 'Location', width: 160 },
+    { field: 'country',headerName: 'Country',width: 160 }
+  ];
+  
+const WorkshopTable = ({ workshops }) => {
+
+    const actionColumn = [
+        {
+          field: "action",
+          headerName: "Action",
+          width: 200,
+          renderCell: (params) => {
+            return (
+              <div className="cellAction">
+                {/* <Link to="/users/test" style={{ textDecoration: "none" }}>
+                  <div className="viewButton">View</div>
+                </Link> */}
+                <button
+                  className="deleteButton"
+                  onClick={() => handleDelete(params.row.id)}
+                >
+                  Delete
+                </button>
+              </div>
+            );
+          },
+        },
+      ];
+
+    const rows = [];
+
+    workshops.map(row => {
+    let count = 1
+    const rowData = {id: row.id, practice: row.practice, date: row.date, location: row.location, price: row.price, country: row.country}
+    rows.push(rowData)
+    count = count + 1
+    })
 
     const handleDelete = async (id) => {
           await deleteDoc(doc(db, "upcoming-workshops", id));
       }
 
     return (
-        <div className='workshop-card-container'>
-            <img src={img} alt="" />
-            <div className='main'>
-                <h2>{practice}</h2>
-                <p>{country}</p>
-                <p>{location}</p>
-                <p>{price}</p>
-                <p>{sessions}</p>
-            </div>
-            <div className='footer'>
-                <button onClick={() => handleDelete(id)}>Delete</button>
-            </div>
-        </div>
+
+        <div className='burger' style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns.concat(actionColumn)}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
+    </div>
     )
 }
 
--- client-side/src/routes/admin/admin-page.component.js
+++ client-side/src/routes/admin/admin-page.component.js
@@ -8,14 +8,11 @@ import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 import { WorkshopsContext } from '../../contexts/workshop.context'
 import WorkshopTable from '../../components/admin-page-components/workshop-table/workshop-table.component'
-import { userInputs } from "../../formSource";
+import AddWorkshop from '../../components/admin-page-components/add-workshops/add-workshop.component'
 import './admin-page.styles.scss';
 
 const Admin = () => {
   const { user, logout } = UserAuth();
-  const [file, setFile] = useState("");
-  const [data, setData] = useState({});
-  const [per, setPerc] = useState(null);
   const navigate = useNavigate()
   const {workshops} = useContext(WorkshopsContext);
 
@@ -30,64 +27,6 @@ const Admin = () => {
     }
   }
 
-  //FORM - UPLOAD FUNCTIONS
-  // upload image
-  useEffect(() => {
-    const uploadFile = () => {
-      const name = new Date().getTime() + file.name;
-      const storageRef = ref(storage, file.name);
-
-      const uploadTask = uploadBytesResumable(storageRef, file);
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log("Upload is " + progress + "% done");
-          setPerc(progress);
-          switch (snapshot.state) {
-            case "paused":
-              console.log("Upload is paused");
-              break;
-            case "running":
-              console.log("Upload is running");
-              break;
-            default:
-              break;
-          }
-        },
-        (error) => {
-          console.log(error);
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            setData((prev) => ({ ...prev, img: downloadURL }));
-          });
-        }
-      );
-    };
-    file && uploadFile();
-  }, [file]);
-  
-  //add workshop
-  const handleInput = (e) => {
-    const id = e.target.id;
-    const value = e.target.value;
-    setData({ ...data, [id]: value });
-  };
-
-  const handleAddWorkshop = async (e) => {
-    e.preventDefault();
-    try {
-      await addDoc(collection(db, "upcoming-workshops"), {
-        ...data,
-        timeStamp: serverTimestamp(),
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
   return (
     <>
     <div className="title-container">
@@ -95,41 +34,16 @@ const Admin = () => {
         <h1 className="title">Current User: {user && user.email}</h1>
         <button onClick={handleLogout}>Logout</button>
     </div>
+    <AddWorkshop />
 
-    <div className='admin-form-container'>
-      <form className='admin-form' onSubmit={handleAddWorkshop}>
-        {userInputs.map((input) => (
-          <div className="formInput" key={input.id}>
-            <label>{input.label}</label>
-            <input
-              id={input.id}
-              type={input.type}
-              placeholder={input.placeholder}
-              onChange={handleInput}
-            />
-          </div>
-        ))}
-        <div className="formInput">
-          <label htmlFor="file">
-            Image: <DriveFolderUploadOutlinedIcon className="icon" />
-          </label>
-          <input
-            type="file"
-            id="file"
-            onChange={(e) => setFile(e.target.files[0])}
-            style={{ display: "none" }}
-          />
-        </div>
-        <button className="send-button" disabled={per !== null && per < 100} type="submit">Send</button>
-      </form>
-    </div>
     <div className="all workshops">
     <h2>All Workshops</h2>
-      <div className="workshops-container">
+      {/* <div className="workshops-container">
         {workshops.map((workshop) => (
           <WorkshopTable key={workshop.id} workshop={workshop} />
         ))}
-      </div>
+      </div> */}
+      <WorkshopTable workshops={workshops}/>
     </div>
     </>
   )
