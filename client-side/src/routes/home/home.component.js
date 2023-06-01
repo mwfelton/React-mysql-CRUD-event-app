95e901c21e41dea93226cd6dc42a030471f201cf
Miles Felton Gerber <mwfelton03@gmail.com>
2023-06-01 14:06:04 +0200

    background hero set

--- client-side/src/routes/home/home.component.js
+++ client-side/src/routes/home/home.component.js
@@ -1,12 +1,18 @@
-import Workshops from "../workshops/workshops-page.component";
+// import Workshops from "../workshops/workshops-page.component";
+// <Workshops />
+
+import './home.styles.css';
 
 const Home = () => (
+  <>
     <div className="title-container">
       <h1 className="title">YOGAAGMA</h1>
       <h2 className="sub-heading">ISHA HATHA YOGA ZURICH,</h2>
       <h2 className="sub-heading">SWITZERLAND</h2>
-      <Workshops />
     </div>
+
+    <div className="section_hero_bg"></div>
+  </>
 );
 
 export default Home;
